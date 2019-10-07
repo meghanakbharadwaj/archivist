5f65bec6079834b159d3cc2df5833270fe1d06b5
Varshith V <30930913+varshith-v@users.noreply.github.com>
2019-10-07 16:05:43 +0530

    addCourse page designed

--- archivist_app/urls.py
+++ archivist_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from archivist_app import views
 
 urlpatterns = [
-    path('',views.home,name = 'home')
+    #path('',views.homeView,name = 'home')
+    path('',views.addCourseView,name = 'addCourse')
 ]
--- archivist_app/views.py
+++ archivist_app/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
+def homeView(request):
     return render(request,'home.html')
+
+def addCourseView(request):
+    return render(request,'addCourse.html')
