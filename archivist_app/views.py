9429d4b9325f003cadd7a0da5db375c0ad169c25
yogeshj939 <32947095+yogeshj939@users.noreply.github.com>
2019-10-09 18:15:11 +0530

    linking domain to keywords

--- archivist_app/views.py
+++ archivist_app/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from archivist_app.models import *
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
-    
+
 def homeView(request):
     return render(request,'home.html')
 
 def addCourseView(request):
     return render(request,'addCourse.html')
+
+def categories(request):
+    keywords = Keywords.objects.all()
+    return render(request,'categories.html',{'keywords':keywords})
