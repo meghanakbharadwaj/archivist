f000400899c9eab88653d14d8c395b6e675f205d
yogeshj939 <32947095+yogeshj939@users.noreply.github.com>
2019-10-25 20:46:49 +0530

    Categories working perfectly.

--- archivist_app/urls.py
+++ archivist_app/urls.py
@@ -4,9 +4,10 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('',views.homeView,name = 'home'),
-    #path('',views.addCourseView,name = 'addCourse'),
-    path('courses',views.categories,name = 'categories'),
+    path('categories/(?P<dom>)',views.categories,name = 'categories'),
     path('courses/(?P<key>[-a-zA-Z0-9_]+)$',views.courses,name='courses'),
     path('courseLink/(?P<course_name>)',views.courseLink,name='courseLink'),
     path('login',views.login,name='login')
 ]
+
+
