1c9ffaecef10f1ed2cd96356f8dded00c97905e8
Varshith V <30930913+varshith-v@users.noreply.github.com>
2019-10-29 16:18:39 +0530

    Provider moving list arrows

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
       autoplay: true,
       autoplaySpeed: 1000,
       arrows: true,
-      dots: true,
+      dots: false,
+      prevArrow:$(".prev"),//"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left fa-4x' aria-hidden='true'></i></button>",
+      nextArrow:$(".next"),//"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right fa-4x' aria-hidden='true'></i></button>",
       pauseOnHover: false,
       responsive: [{
         breakpoint: 768,
